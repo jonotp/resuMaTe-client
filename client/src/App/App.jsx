c02eeaae5f662a9d470d381ab843825643838235
Jono <jonathanphame@gmail.com>
2020-11-05 17:41:07 +1100

    Add react app from scratch
    Installed the following:
    - webpack, webpack cli: for bundling
    -webpack-dev-server: for hot reloading and running the app in dev
    - babel*: to transpile modern js to backwards compatible versions. It will also transform jsx to js (for react)
    -- note .babelrc runs right to left and will convert jsx to js then js to backwards compatible js
    - babel-loader: is loader used by webpack to handle jsx and js file
    - style-loader, css-loader, sass-loader: used by webpack to handle scss -> css -> styles
    - file-loader: used by webpack to handle image files
    https://github.com/sapinder-pal/React-Webpack-Starter-with-Babel-Sass

---
+++ client/src/App/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./app.scss";
+
+function App() {
+	return (
+		<div>
+			<h1>My React App2</h1>
+		</div>
+	)
+}
+
+export default App;
---
+++ client/webpack.config.js
@@ -0,0 +1,44 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+module.exports = {
+  devServer: {
+    hot: true,
+    open: true,
+    port: 3000,
+  },
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    path: path.resolve("dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    // Will handle creating a new html in the build directory
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCSSExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
+};

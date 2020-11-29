85651c8fd0e39d5ebe60ce58cde14b2f54304401
Jono <jonathanphame@gmail.com>
2020-11-29 12:20:39 +1100

    Use different env file depending on webpack mode

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -4,58 +4,64 @@ const dotenv = require("dotenv");
 const path = require("path");
 const webpack = require("webpack");
 
-const env = dotenv.config().parsed;
+module.exports = (_, { mode }) => {
+  const envFile =
+    mode === "development"
+      ? path.join(__dirname, ".env")
+      : path.join(__dirname, ".env.production");
+  const env = dotenv.config({ path: envFile }).parsed;
 
-// reduce it to a nice object, the same as before
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
+  // reduce it to a nice object, the same as before
+  const envKeys = Object.keys(env).reduce((prev, next) => {
+    prev[`process.env.${next}`] = JSON.stringify(env[next]);
+    return prev;
+  }, {});
 
-module.exports = {
-  devServer: {
-    hot: true,
-    open: true,
-    port: 3000,
-    historyApiFallback: true,
-  },
-  entry: {
-    index: "./src/index.js",
-  },
-  output: {
-    path: path.resolve("dist"),
-    filename: "main.js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js|\.jsx$/,
-        exclude: /node_modules/,
-        use: "babel-loader",
-      },
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: ["file-loader"],
-      },
+  return {
+    devServer: {
+      hot: true,
+      open: true,
+      port: 3000,
+      historyApiFallback: true,
+    },
+    entry: {
+      index: "./src/index.js",
+    },
+    output: {
+      path: path.resolve("dist"),
+      filename: "main.js",
+      publicPath: "/",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js|\.jsx$/,
+          exclude: /node_modules/,
+          use: "babel-loader",
+        },
+        {
+          test: /\.scss$/,
+          use: ["style-loader", "css-loader", "sass-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: ["file-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx", ".scss"],
+    },
+    plugins: [
+      new webpack.EnvironmentPlugin(envKeys),
+      // Will handle creating a new html in the build directory
+      new HTMLWebpackPlugin({
+        template: "./src/index.html",
+      }),
+      new MiniCSSExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      }),
     ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx", ".scss"],
-  },
-  plugins: [
-    new webpack.EnvironmentPlugin(envKeys),
-    // Will handle creating a new html in the build directory
-    new HTMLWebpackPlugin({
-      template: "./src/index.html",
-    }),
-    new MiniCSSExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
-    }),
-  ],
+  };
 };

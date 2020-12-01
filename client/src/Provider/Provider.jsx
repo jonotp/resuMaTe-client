5c3f599b48401495a2811e75ad2e17fdcc37d54d
Jono <jonathanphame@gmail.com>
2020-12-02 10:26:35 +1100

    Add auth context & route protection with HO func
    WithAuthorization can be used to check if certain conditions are satisfied to display the component. If not, it will push the user to the login screen. May implement a ProtectedRoute component instead.....

--- client/src/Provider/Provider.jsx
+++ client/src/Provider/Provider.jsx
@@ -4,18 +4,21 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 import FirebaseProvider from "../Firebase/Firebase.Provider.jsx";
-import theme from '../theme.js';
+import AuthenticationProvider from "../Authentication/Authentication.Provider.jsx";
+import theme from "../theme.js";
 
 function Provider({ children }) {
   return (
-    <FirebaseProvider>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          {children}
-        </MuiPickersUtilsProvider>
-      </ThemeProvider>
-    </FirebaseProvider>
+    <AuthenticationProvider>
+      <FirebaseProvider>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            {children}
+          </MuiPickersUtilsProvider>
+        </ThemeProvider>
+      </FirebaseProvider>
+    </AuthenticationProvider>
   );
 }
 

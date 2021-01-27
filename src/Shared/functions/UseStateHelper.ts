5c92f84baf9e6dd969f17b070d73a8737f748526
Jono <jonathanphame@gmail.com>
2021-01-27 17:32:09 +1100

    Add use state helper functions with ID interface

---
+++ src/Shared/functions/UseStateHelper.ts
@@ -0,0 +1,40 @@
+import React, { ChangeEvent } from "react";
+import { ID } from "../Interfaces/ID.interface";
+import { v4 as uuidv4 } from "uuid";
+
+export const UseStateHelperFormInputChange = <T>(dispatch: React.Dispatch<React.SetStateAction<T>>) => (event: ChangeEvent<HTMLInputElement>) => {
+  event.persist();
+  return (
+    dispatch((prev: T) => {
+      return {
+        ...prev,
+        [event.target.name]: event.target.value,
+      };
+    })
+  )
+};
+
+export const UseStateHelperArrayElementIDInputChange = <T extends ID>(dispatch: React.Dispatch<React.SetStateAction<T[]>>) => (id: string) => (event: ChangeEvent<HTMLInputElement>) => {
+  event.persist();
+  return (
+    dispatch((prev: T[]) => prev.map((x) => x.id === id ? { ...x, [event.target.name]: event.target.value } : x))
+  )
+};
+
+export const UseStateHelperArrayElementIDChange = <T extends ID>(dispatch: React.Dispatch<React.SetStateAction<T[]>>) => (id: string) => (property: string, value: any) => {
+  return (
+    dispatch((prev: T[]) => prev.map((x) => x.id === id ? { ...x, [property]: value } : x))
+  )
+};
+
+export const UseStateHelperArrayElementIDDelete = <T extends ID>(dispatch: React.Dispatch<React.SetStateAction<T[]>>) => (id: string) => () => {
+  return (
+    dispatch((prev: T[]) => prev.filter((x) => x.id !== id))
+  )
+};
+
+export const UseStateHelperArrayElementIDAdd = <T extends ID>(dispatch: React.Dispatch<React.SetStateAction<T[]>>) => (item: T) => () => {
+  return (
+    dispatch((prev: T[]) => prev.concat({ ...item, id: uuidv4() }))
+  )
+};
